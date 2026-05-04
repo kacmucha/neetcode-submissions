735339ffd33147dbcf5a80be30f2f87a23e2031f
Kacper Mucha <99989097+kacmucha@users.noreply.github.com>
2026-05-04 14:42:06 +0200

    Add: singlyLinkedList - submission-18

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-18.py
@@ -0,0 +1,71 @@
+class Node:
+
+    def __init__(self, val=0, next=None):
+        self.val = val;
+        self.next = next;
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.guard = Node(None, None)
+
+    def get(self, index: int) -> int:
+        curr = self.guard.next
+
+        i = 0
+        while curr:
+            if i == index:
+                return curr.val
+            curr = curr.next
+            i += 1
+        return -1
+
+    def insertHead(self, val: int) -> None:
+        newNode = Node(val)
+        if self.guard.next:
+            newNode.next = self.guard.next
+
+        self.guard.next = newNode
+
+        if self.tail == None:
+            self.tail = newNode
+
+    def insertTail(self, val: int) -> None:
+        newNode = Node(val)
+        if self.tail == None:
+            self.guard.next = newNode
+            self.head = newNode
+            self.tail = newNode
+            return
+
+        self.tail.next = newNode
+        self.tail = newNode
+        
+
+    def remove(self, index: int) -> bool:
+        self.head = self.guard
+        for i in range(0, index):
+            if self.head.next:
+                self.head = self.head.next
+            else:
+                return False
+        
+        if self.head.next:
+            if self.head.next.next:
+                self.head.next = self.head.next.next
+            else:
+                self.head.next = None
+                self.tail = self.head
+            return True
+        return False
+
+    def getValues(self) -> List[int]:
+        resList = []
+        self.head = self.guard
+        while self.head.next:
+            self.head = self.head.next
+            resList.append(self.head.val)
+        
+        return resList
